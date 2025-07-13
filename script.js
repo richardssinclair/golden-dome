80937ba248bf8c1f89bbebc2e308d3764d94620f
Richard Sinclair <richardsinclair@MacBookPro.communityfibre.co.uk>
2025-07-13 12:35:58 +0100

    class fixes, style tweeks

--- script.js
+++ script.js
@@ -9,10 +9,79 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap.ticker.add((time) => lenis.raf(time * 1000));
   gsap.ticker.lagSmoothing(0);
 
+  // Gold Dome intro zoom
+  const cards = gsap.utils.toArray(".card");
+  const introCard = cards[0];
+
+  const cardImageWrapper = introCard.querySelector(".card-img");
+  const cardImg = introCard.querySelector(".card-img img");
+  gsap.set(cardImageWrapper, {
+    scale: 0.5,
+    borderRadius: "600px 600px 50px 50px",
+  });
+  gsap.set(cardImg, { scale: 1.5 });
+  // function animateContentIn(titleChars, description) {
+  //   gsap.to(titleChars, { x: "0%", duration: 0.75, ease: "power4.out" });
+  //   gsap.to(description, {
+  //     x: 0,
+  //     opacity: 1,
+  //     duration: 0.75,
+  //     delay: 0.1,
+  //     ease: "power4.out",
+  //   });
+  // }
+
+  ScrollTrigger.create({
+    trigger: introCard,
+    start: "top top",
+    end: "+=300vh",
+    onUpdate: (self) => {
+      const progress = self.progress;
+      const imgScale = 0.5 + progress * 0.5;
+      const borderRadius = 400 - progress * 375;
+      const innerImgScale = 1.5 - progress * 0.5;
+
+      gsap.set(cardImageWrapper, {
+        scale: imgScale,
+        borderRadius: `${borderRadius}px ${borderRadius}px 50px 50px`,
+      });
+      gsap.set(cardImg, { scale: innerImgScale });
+    },
+  });
+
+  // Make Donald golden
+  gsap.set(".donald-img-2", { opacity: 0 });
+  const donaldGold = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".donald",
+      start: "center 50%",
+      end: "bottom 50%",
+      scrub: true,
+    },
+  });
+
+  donaldGold.to(".donald-img-2", { opacity: 1, duration: 1 });
+
+  // Map colouring in functionality
+  gsap.set("#map-2", { opacity: 0 });
+  gsap.set("#map-3", { opacity: 0 });
+
+  const mapTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".map",
+      start: "top 70%",
+      end: "bottom 80%",
+      scrub: true,
+    },
+  });
+
+  mapTimeline
+    .to("#map-2", { opacity: 1, duration: 0.5 })
+    .to("#map-3", { opacity: 1, duration: 1 });
+
   // Changing city names
   const cities = ["London", "Paris", "Tokyo", "New York", "Beijing", "Berlin"];
   const cityElement = document.querySelector(".city");
-  console.log("cityElement", cityElement);
   let index = 0;
 
   setInterval(() => {
@@ -39,47 +108,8 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
-  const cards = gsap.utils.toArray(".card");
-  const introCard = cards[0];
-
-  // I think this is making the dome bigger
-  const cardImageWrapper = introCard.querySelector(".card-img");
-  const cardImg = introCard.querySelector(".card-img img");
-  gsap.set(cardImageWrapper, {
-    scale: 0.5,
-    borderRadius: "600px 600px 50px 50px",
-  });
-  gsap.set(cardImg, { scale: 1.5 });
-  function animateContentIn(titleChars, description) {
-    gsap.to(titleChars, { x: "0%", duration: 0.75, ease: "power4.out" });
-    gsap.to(description, {
-      x: 0,
-      opacity: 1,
-      duration: 0.75,
-      delay: 0.1,
-      ease: "power4.out",
-    });
-  }
-
-  // Map colouring in functionality
-  gsap.set("#map-2", { opacity: 0 });
-  gsap.set("#map-3", { opacity: 0 });
-
-  const mapTimeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".map",
-      start: "top 70%",
-      end: "bottom 80%",
-      scrub: true,
-    },
-  });
-
-  mapTimeline
-    .to("#map-2", { opacity: 1, duration: 0.5 })
-    .to("#map-3", { opacity: 1, duration: 1 });
-
   // rocket flying from left to right
-  gsap.to(".rocket-speed-img", {
+  gsap.to(".red-rocket-img", {
     x: "100vw",
     ease: "none",
     scrollTrigger: {
@@ -87,37 +117,6 @@ document.addEventListener("DOMContentLoaded", () => {
       start: "top bottom",
       end: "bottom top",
       scrub: true,
-    },
-  });
-
-  // Make Donald golden
-  gsap.set(".donald-img-2", { opacity: 0 });
-  const donaldGold = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".donald",
-      start: "center 50%",
-      end: "bottom 50%",
-      scrub: true,
-    },
-  });
-
-  donaldGold.to(".donald-img-2", { opacity: 1, duration: 1 });
-
-  ScrollTrigger.create({
-    trigger: introCard,
-    start: "top top",
-    end: "+=300vh",
-    onUpdate: (self) => {
-      const progress = self.progress;
-      const imgScale = 0.5 + progress * 0.5;
-      const borderRadius = 400 - progress * 375;
-      const innerImgScale = 1.5 - progress * 0.5;
-
-      gsap.set(cardImageWrapper, {
-        scale: imgScale,
-        borderRadius: `${borderRadius}px ${borderRadius}px 50px 50px`,
-      });
-      gsap.set(cardImg, { scale: innerImgScale });
     },
   });
 });
